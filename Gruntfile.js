084c351440959c9d3cf45e68f9175ae7bd0fe3d7
Nay Zaw Oo <nayzawoo@yandex.com>
2016-10-03 23:51:18 +0630

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function( grunt ) {
+
+	grunt.initConfig( {
+
+		// Import package manifest
+		pkg: grunt.file.readJSON( "package.json" ),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.license %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			options: {
+				banner: "<%= meta.banner %>"
+			},
+			dist: {
+				src: [ "src/js/jquery.accordion.js" ],
+				dest: "dist/js/jquery.accordion.js"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: [ "src/js/jquery.accordion.js", "test/**/*" ],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			dist: {
+				src: [ "dist/js/jquery.accordion.js" ],
+				dest: "dist/js/jquery.accordion.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// watch for changes to source
+		// Better than calling grunt a million times
+		// (call 'grunt watch')
+		watch: {
+			files: [ "src/*", "test/**/*" ],
+			tasks: [ "default" ]
+		}
+
+	} );
+
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+	grunt.registerTask( "travis", [ "jshint"] );
+	grunt.registerTask( "lint", [ "jshint" ] );
+	grunt.registerTask( "build", [ "concat", "uglify" ] );
+	grunt.registerTask( "default", [ "jshint", "build"] );
+};
